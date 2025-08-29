6880e1b1642affa89c832f1cc625d41dc8c8979e
lsces <lester@lsces.co.uk>
2025-08-29 17:16:12 +0100

    This functions are probably obsolite as get_user_avatar does not exist but leaving for moment

--- show_user_avatar.php
+++ show_user_avatar.php
@@ -11,16 +11,16 @@
 /**
  * required setup
  */
-include_once( USERS_PKG_INCLUDE_PATH.'userprefs_lib.php' );
+global $gBitSystem, $gBitUser;
+
 // application to display an image from the database with
 // option to resize the image dynamically creating a thumbnail on the fly.
 // you have to check if the user has permission to see this gallery
 if (!isset($_REQUEST["user"])) {
 	die;
 }
-$info = $userprefslib->get_user_avatar_img($_REQUEST["user"]);
+$info = $gBitUser->get_user_avatar( $_REQUEST["user"] );
 $type = $info["avatar_file_type"];
 $content = $info["avatar_data"];
 header ("Content-type: $type");
-echo "$content";
-?>
+echo (string) $content;
